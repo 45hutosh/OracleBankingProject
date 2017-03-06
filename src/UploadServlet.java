aea946ca64450c7ce8c7240b2c7ff6c4fd04b7b2
Ashutosh <ashu.telrandhe@gmail.com>
2017-03-06 08:46:09 +0530

    UploadServlet Clean Up

--- src/UploadServlet.java
+++ src/UploadServlet.java
@@ -3,9 +3,6 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.List;
 
@@ -18,7 +15,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.apache.commons.io.FileUtils;
 
 /**
  * Servlet implementation class UploadServlet
@@ -41,22 +37,9 @@ public class UploadServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 
 		File file;
-		 String fileName=null;
+		String fileName=null;
 		res.setContentType("text/html");  
-		PrintWriter out = res.getWriter();   
-/*		try{ 
-			MultipartRequest multipart=new MultipartRequest(req,"c:/ServletUploads"); 
-			Part filePart = (Part) req.getPart("file");
-			String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
-			System.out.println();
-			//System.out.println(filename[0]);
-		
-			
-		}
-		
-		catch(Exception e ){
-			System.out.println("Exception In MultiPart");
-		}*/
+		PrintWriter out = res.getWriter();  
 		
 		DiskFileItemFactory factory = new DiskFileItemFactory();
 		factory.setSizeThreshold(1024);
@@ -76,22 +59,23 @@ public class UploadServlet extends HttpServlet {
 			            // Get the uploaded file parameters
 			            fileName = fi.getName();
 			            // Write the file
-			            System.out.println("filename is " + fileName);
+			            //System.out.println("filename is " + fileName);
 			            if( fileName.lastIndexOf("\\") >= 0 ){
 			               file = new File("D:/ServletUploads/"+fileName.substring( fileName.lastIndexOf("\\"))) ;
 			            }else{
 			               file = new File("D:/ServletUploads/"+fileName.substring(fileName.lastIndexOf("\\")+1)) ;
 			            }
+			            
 			            File tmpFile= new File("D:/ServletUploads/" + file.getName());
 			            if(tmpFile.exists())
-			            {
-			            	System.out.println("File already exists with same name.. Deleting old file..");
-			            	//tmpFile.delete();
-			            }
+				            {
+				            	System.out.println("File already exists with same name. Deleting old file.");
+				            	//tmpFile.delete();
+				            }
 			            fi.write( file ) ;
 			            System.out.println(file.getName());
 			            
-			          out.println("Uploaded Filename: " + fileName + "<br>");
+			          //out.println("Uploaded Filename: " + fileName + "<br>");
 			         }
 			      }
 	
@@ -100,7 +84,7 @@ public class UploadServlet extends HttpServlet {
 			       System.out.println(ex);
 			       ex.printStackTrace();
 			   }
-		   req.setAttribute("filename", fileName);
+		req.setAttribute("filename", fileName);
 		RequestDispatcher rd=req.getRequestDispatcher("readfile");  		  
 		rd.forward(req, res);
 	
